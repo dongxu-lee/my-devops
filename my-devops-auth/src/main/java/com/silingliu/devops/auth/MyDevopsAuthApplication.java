aa1635083e1f4ba9d28ae9177cd737c25c15db0e
lidongxu <1343635850@qq.com>
2023-03-19 19:31:53 +0800

    .

---
+++ my-devops-auth/src/main/java/com/silingliu/devops/auth/MyDevopsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.silingliu.devops.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevopsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDevopsAuthApplication.class, args);
+    }
+
+}
---
+++ my-devops-auth/src/test/java/com/silingliu/devops/auth/MyDevopsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silingliu.devops.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevopsAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
